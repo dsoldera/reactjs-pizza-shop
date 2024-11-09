285f8f3692064fc250e7626276485f27e6762fd3
dsoldera <daniellesoldera@gmail.com>
2024-11-09 15:20:57 -0300

    finish the layout

--- pizza-shop/src/router.tsx
+++ pizza-shop/src/router.tsx
@@ -2,7 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
+import { NotFound } from './pages/app/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -10,15 +12,16 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
         element: <Dashboard />,
       },
-      // {
-      //   path: '/orders',
-      //   element: <Orders />,
-      // },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
     ],
   },
   {
