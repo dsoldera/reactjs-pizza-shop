c5c4e7762e49856aca5f559e6393129bb4ab6162
dsoldera <daniellesoldera@gmail.com>
2024-11-11 13:24:08 -0300

    install axios and react-query and start to create the hooks

---
+++ pizza-shop/src/api/register-restaurant.ts
@@ -0,0 +1,16 @@
+import { api } from '@/lib/axios'
+
+export interface RegisterRestaurantBody {
+  restaurantName: string
+  managerName: string
+  email: string
+  phone: string
+}
+export const registerRestaurant = async ({
+  restaurantName,
+  managerName,
+  email,
+  phone,
+}: RegisterRestaurantBody) => {
+  await api.post('/restaurants', { email, managerName, phone, restaurantName })
+}
